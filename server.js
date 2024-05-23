4729b894841cdf49f3de1130f7024ece182306e4
Fiddy <thnhlong2112@gmail.com>
2024-05-23 20:34:30 +0700

    feat: create mintNFT

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const starton = axios.create({
 });
 
 app.post("/upload", cors(), upload.single("file"), async (req, res) => {
-  let data = FormData();
+  let data = new FormData();
   const blob = new Blob([req.file.buffer], { type: req.file.mimetype });
   data.append("file", blob, { filename: req.file.originalname });
   data.append("isSync", "true");
@@ -34,7 +34,7 @@ app.post("/upload", cors(), upload.single("file"), async (req, res) => {
   async function uploadImgOnIpfs() {
     const ipfsImg = await starton.post("/v3/ipfs/file", data, {
       header: {
-        "Content-type": `multipart/form-data; boundary=${data.getBoundary()}`,
+        "Content-type": `multipart/form-data; boundary=${data._boundary}`,
       },
     });
     return ipfsImg.data;
@@ -48,7 +48,7 @@ app.post("/upload", cors(), upload.single("file"), async (req, res) => {
       image: `ipfs://ipfs/${imgId}`,
     };
 
-    const ipfsMetadata = await starton.port("/v3/ipfs/json", {
+    const ipfsMetadata = await starton.post("/v3/ipfs/json", {
       name: "My NFT Metadata JSON",
       content: metadataJson,
       isSync: true,
@@ -56,7 +56,29 @@ app.post("/upload", cors(), upload.single("file"), async (req, res) => {
     return ipfsMetadata.data;
   }
 
+  async function mintNFT(receiverAddress, metadataCid) {
+    const nft = await starton.post(
+      `/v3/smart-contract/${process.env.SMART_CONTRACT_NETWORK}/${process.env.SMART_CONTRACT_ADDRESS}/call`,
+      {
+        functionName: "mint",
+        signerWallet: `${process.env.WALLET_IMPORT_ON_STARTON}`,
+        speed: "low",
+        params: [receiverAddress, metadataCid],
+      }
+    );
+    return nft.data;
+  }
+  const RECEIVER_ADDRESS = "0x834D1A80154FB0497E0aA995b90c27572ADE89c8";
   const ipfsImgData = await uploadImgOnIpfs();
   const ipfsMetadata = await uploadDataOnIpfs(ipfsImgData.cid);
-  console.log(ipfsImgData, ipfsMetadata);
+  const mint = await mintNFT(RECEIVER_ADDRESS, ipfsMetadata.cid);
+  // console.log(ipfsImgData, ipfsMetadata);
+  res.status(200).json({
+    transactionHash: mint.transactionHash,
+    cid: ipfsImgData.cid,
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
